2da819059ca989a25cda997f0d7e38d77a701d5f
hrdrq <son.munehiro+1@gmail.com>
2019-05-18 11:06:44 +0900

    play

--- rt/ui/list/list.py
+++ rt/ui/list/list.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QListWidget
+from PyQt5.QtWidgets import QListWidget, QListWidgetItem
+
+class Item(QListWidgetItem):
+
+    def __init__(self, title, id):
+        super().__init__()
+        self.setText(title)
+        self.id = id
 
 class List(QListWidget):
 
-    def __init__(self, article_list):
+    def __init__(self, rt, article_list):
         super().__init__()
+        self.rt = rt
         for article in article_list:
-            self.addItem(article['title'])
+            item = Item(article['title'], article['id'])
+            self.addItem(item)
+        self.itemClicked.connect(self.select)
+
+    def select(self, item):
+        article = self.rt.article.item(item.id)
+        self.rt.ui.view.set_article(article)
--- rt/ui/view/view.py
+++ rt/ui/view/view.py
@@ -1,6 +1,39 @@
 # -*- coding: utf-8 -*-
 
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QLabel, QPushButton
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtCore import QUrl
+
+class Play(QPushButton):
+
+    def __init__(self, parent, start, end):
+        super().__init__(">", parent)
+        self.start = start
+        self.end = end
+        self.clicked.connect(self.do)
+        self.player = QMediaPlayer(self)
+        sound = QMediaContent(QUrl.fromLocalFile("/Users/ericsun/Downloads/20190428.mp3"))
+        self.player.setMedia(sound)
+        self.player.setVolume(50)
+
+    def do(self):
+        print('do', self.start, self.end)
+        self.player.play()
+        print('end')
+
+class Sentence(QWidget):
+
+    def __init__(self, sentence):
+        super().__init__()
+        self.layout = QGridLayout()
+        self.play = Play(self, sentence['start'], sentence['end'])
+        self.text = QLabel(sentence['text'], self)
+        self.text.move(50, 0)
+        # text.setText()
+
+        # self.layout.addWidget(button, 0, 0)
+        # self.layout.addWidget(text, 0, 1)
+        # self.setLayout(self.layout)
 
 class Paragraph(QWidget):
 
@@ -14,27 +47,21 @@ class Paragraph(QWidget):
             self.layout.addWidget(sentence)
         self.setLayout(self.layout)
 
-class Sentence(QWidget):
-
-    def __init__(self, sentence):
-        super().__init__()
-        self.layout = QHBoxLayout()
-        text = QLabel()
-        text.setText(sentence['text'])
-        button = QPushButton(">")
-        self.layout.addWidget(button)
-        self.layout.addWidget(text)
-        self.setLayout(self.layout)
-
 class View(QWidget):
 
-    def __init__(self, article):
+    def __init__(self, rt, article):
         super().__init__()
+        self.rt = rt
         self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.set_article(article)
+
+    def set_article(self, article):
+        # if not hasattr(self, 'layout'):
+        for i in reversed(range(self.layout.count())):
+            self.layout.itemAt(i).widget().setParent(None)
         self.paragraphs = []
         for _paragraph in article:
             paragraph = Paragraph(_paragraph)
             self.paragraphs.append(paragraph)
             self.layout.addWidget(paragraph)
-
-        self.setLayout(self.layout)
