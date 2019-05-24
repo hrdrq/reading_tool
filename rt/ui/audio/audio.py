d4a5c55e1d841c32716194e57c7193154bd430ac
hrdrq <son.munehiro+1@gmail.com>
2019-05-24 23:10:10 +0900

    new scroll

--- rt/ui/audio/audio.py
+++ rt/ui/audio/audio.py
@@ -3,7 +3,7 @@
 import json
 from threading import Thread
 
-from PyQt5.QtWidgets import QWidget, QScrollArea
+from PyQt5.QtWidgets import QWidget, QScrollArea, QVBoxLayout
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('Qt5Agg')
@@ -18,28 +18,48 @@ class Audio(QWidget):
     def __init__(self, parent, path):
         super().__init__()
         self.parent = parent
-        self.scroll = QScrollArea(self)
+        # self.scroll = QScrollArea(self)
         self.fig, self.ax = plt.subplots(1)
         DPI = self.fig.get_dpi()
-        self.fig.set_size_inches(7200.0/float(DPI),190.0/float(DPI))
+        # self.fig.set_size_inches(7200.0/float(DPI),190.0/float(DPI))
         self.figure_canvas = FigureCanvas(self.fig)
-        self.figure_canvas.setParent(parent)
-        self.scroll.setWidget(self.figure_canvas)
-        self.scroll.setFixedHeight(200)
-        self.scroll.setFixedWidth(750)
-        self.setFixedHeight(200)
+        self.figure_canvas.setParent(self)
+        # self.scroll.setWidget(self.figure_canvas)
+        # self.scroll.setFixedHeight(200)
+        # self.scroll.setFixedWidth(750)
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.figure_canvas)
+        # self.setFixedWidth(750)
+        self.setFixedHeight(300)
         sound = AudioSegment.from_file(path, "mp3")
         data = np.array(sound.get_array_of_samples())
         data = data[::sound.channels]
-        self.ax.plot(data[::int(sound.frame_rate / 100)])
-
+        self.data = data[::int(sound.frame_rate / 100)]
+        self.offset = 0
+        self.ax.plot(range(self.offset, self.offset + 2000), self.data[self.offset:self.offset + 2000])
+        self.ax.get_yaxis().set_visible(False)
         self.start = None
         self.end = None
         self.fill = None
+        self.fig.canvas.mpl_connect('scroll_event', self.on_scroll)
         self.fig.canvas.mpl_connect('button_press_event', self.on_press)
         self.fig.canvas.mpl_connect('button_release_event', self.on_release)
 
         self.player = Player(path)
+
+    def on_scroll(self, event):
+        print(event)
+        if event.button == 'down' and self.offset < len(self.data) - 50:
+            self.offset += 50
+        elif event.button == 'up' and self.offset > 50:
+            self.offset -= 50
+        else:
+            return
+        self.fill = None
+        self.ax.clear()
+        plot_data = self.data[self.offset:self.offset + 2000]
+        self.ax.plot(range(self.offset, self.offset + len(plot_data)), plot_data)
+        self.fig.canvas.draw()
 
     def on_press(self, event):
         self.start = int(event.xdata)
